50b1fe8650735a61b477e9434b4a67e95de48979
XBEAST1 <146877045+dimitrescuwallpapers@users.noreply.github.com>
2023-11-13 02:33:16 +0500

    V5
    Performance Increased For Every Type of Device

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,60 +96,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   /**
-   * Init swiper slider with 1 slide at once in desktop view
-   */
-  new Swiper('.slides-1', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
-
-  /**
-   * Init swiper slider with 3 slides at once in desktop view
-   */
-  new Swiper('.slides-3', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
-      },
-
-      1200: {
-        slidesPerView: 3,
-      }
-    }
-  });
-
-  /**
    * Animation on scroll function and init
    */
   function aos_init() {
@@ -166,13 +112,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-  window.addEventListener("load", adjustGalleryLayout);
-  window.addEventListener("resize", adjustGalleryLayout);
+window.addEventListener("load", adjustGalleryLayout);
+window.addEventListener("resize", adjustGalleryLayout);
 
-  function adjustGalleryLayout() {
+function adjustGalleryLayout() {
     var gallery = document.querySelector(".gallery");
-    var msnry = new Masonry(gallery, {
-      itemSelector: '.gallery-item',
-      gutter: 15,
-    });
+    if (gallery) {
+        var msnry = new Masonry(gallery, {
+            itemSelector: '.gallery-item',
+            gutter: 15,
+        });
+      }
   }
